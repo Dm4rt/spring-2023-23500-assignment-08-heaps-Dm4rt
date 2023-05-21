b9307437a6c1c0567cdbdacf51c88cc3d2f060c3
dm4rt <danmartinez2004@gmail.com>
2023-05-21 11:07:43 -0400

    finished heapsort

--- heap.cpp
+++ heap.cpp
@@ -8,6 +8,15 @@ void heapsort(std::vector <int> &arr){
 	buildHeap(arr,size);
 	
 	//now we sort
+	//first swap largest to back
+	for(int i=size-1;i>=0;i--){
+		//first swap largest to back
+		std::swap(arr[i],arr[0]);
+		
+		//then heapify all elements execept last one
+		heapify(arr,i,0);
+	}
+	
 }
 void buildHeap(std::vector <int> &arr, int size){
 	for( int index = (size/2)-1; index >= 0; index--){
